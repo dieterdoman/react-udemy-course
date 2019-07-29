da44639434001acf5917cecdac852c40cd5aa274
Dieter Doman <dieter@jemstep.com>
2019-07-29 15:57:01 +0200

    Added state to the app.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
-import  Person from './Person/Person'
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React App</h1>
-        <p>It is working</p>
-        <Person name="Name1" age="10" />
-        <Person name="Name2" age="13" >My hobbies: Racing</Person>
-        <Person name="Name3" age="16" />
-      </header>
-    </div>
-  );
+class App extends Comment {
+    state = {
+      persons: [
+          { name: 'Name1', age: 10 },
+          { name: 'Name2', age: 13 },
+          { name: 'Name3', age: 16 }
+      ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>React App</h1>
+                    <p>It is working</p>
+                    <button>Switch Name</button>
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
