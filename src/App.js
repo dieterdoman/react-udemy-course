6f24437370c86f3deffcd987a89273471ece8aa2
Dieter Doman <dieter@jemstep.com>
2019-07-29 15:34:35 +0200

    Added person component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import  Person from './Person/Person'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>React App</h1>
         <p>It is working</p>
+        <Person/>
       </header>
     </div>
   );
