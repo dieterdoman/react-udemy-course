6536557b4d316d789ef7e9cfb19af565217b4cf1
Dieter Doman <dieter@jemstep.com>
2019-07-29 16:11:48 +0200

    Update the state in onclick event.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
     state = {
-      persons: [
-          { name: 'Name1', age: 10 },
-          { name: 'Name2', age: 13 },
-          { name: 'Name3', age: 16 }
-      ]
+        persons: [
+            {name: 'Name1', age: 10},
+            {name: 'Name2', age: 13},
+            {name: 'Name3', age: 16}
+        ]
     };
 
     switchNameHandler = () => {
-          console.log("was clicked");
+        this.setState({
+            persons: [
+                {name: 'Full Name1', age: 10},
+                {name: 'Name2', age: 13},
+                {name: 'Name3', age: 20}
+            ]
+        });
     };
 
     render() {
@@ -22,8 +28,9 @@ class App extends Component {
                     <h1>React App</h1>
                     <p>It is working</p>
                     <button onClick={this.switchNameHandler}>Switch Name</button>
-                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies:
+                        Racing</Person>
                     <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
                 </header>
             </div>
