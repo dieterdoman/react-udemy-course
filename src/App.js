4135f8ff00902ae2dcb63e2e3356710a891607df
Dieter Doman <dieter@jemstep.com>
2019-07-29 15:48:12 +0200

    Displayed the children through props in person component.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1>React App</h1>
         <p>It is working</p>
         <Person name="Name1" age="10" />
-        <Person name="Name2" age="13" >My hobbies: Racing </Person>
+        <Person name="Name2" age="13" >My hobbies: Racing</Person>
         <Person name="Name3" age="16" />
       </header>
     </div>
