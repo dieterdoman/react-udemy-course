b3c2fd55fd4fd6021a2f890a2a33cba2be78908c
Dieter Doman <dieter@jemstep.com>
2019-07-31 14:44:55 +0200

    Added proptypes to person components.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Person.module.css'
 import WithClass from "../../../hoc/WithClass";
+import PropTypes from 'prop-types';
 
 const person = (props) => {
     return (
@@ -10,6 +11,13 @@ const person = (props) => {
             <input type="text" onChange={props.changed} value={props.name}/>
         </WithClass>
     );
+};
+
+person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default person;
