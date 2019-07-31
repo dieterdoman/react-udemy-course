0c81ff6a358994ef02378ecd0a701d6f28e2834e
Dieter Doman <dieter@jemstep.com>
2019-07-31 14:59:40 +0200

    Added ref react hook to person to focus the last person input box, also use useEffect to execute after it was rendered.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import styles from './Person.module.css'
 import WithClass from "../../../hoc/WithClass";
 import PropTypes from 'prop-types';
 
-const person = (props) => {
+const Person = (props) => {
+    const inputFocusRef = useRef(null);
+
+    useEffect(() => {
+        inputFocusRef.current.focus();
+    });
+
     return (
         <WithClass classes={styles.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <input ref={inputFocusRef} type="text" onChange={props.changed} value={props.name}/>
         </WithClass>
     );
 };
 
-person.propTypes = {
+Person.propTypes = {
     click: PropTypes.func,
     name: PropTypes.string,
     age: PropTypes.number,
     changed: PropTypes.func
 };
 
-export default person;
+export default Person;
